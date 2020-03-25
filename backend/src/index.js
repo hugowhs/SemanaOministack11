ae36c2cd08e11d401752d2b7a9e48efd764f4e78
Hugo Hiroshi <hugo.wahasugui@gmail.com>
2020-03-25 01:26:52 -0300

    Origin
    https://github.com/hugowhs/SemanaOministack11.git

---
+++ backend/src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require("./routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+/*
+    Rota / Recursos
+
+    Métodos HTTP:
+
+    GET: Buscar/Listar uma informação do back-end
+    POST: Criar uma informação no back-end
+    PUT: Alterar uma informação no back-end
+    DEKETE: Deletar uma informação no back-end
+
+    Tipos de parâmetros:
+
+    Querry Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+    Route Params: Parâmetros utilizados para identificar recursos
+    Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+
+    Banco de dados:
+
+    SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+    NoSQL: MongoDB, CouchDB, etc
+
+    Driver: SELECT * FROM users
+    Query Builder: table('users').select('*').where()
+    
+*/
+
+app.listen(3333);
